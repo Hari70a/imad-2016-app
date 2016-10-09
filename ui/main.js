1207fe88c10bbb0f8ff09e1e828608947e5d02dd
Hariraj <raja70a@gmail.com>
2016-10-09 19:17:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var image = document.getElementById('madi');
+var image = document.getElementById('madi_img');
 image.onclick = function () {
     console.log(image,"$$$$$")
     image.style.marginLeft='100px';
