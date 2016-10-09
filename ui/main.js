b724b3dfd6bf9ba06e7d23c526d9675b1cace343
Hariraj <raja70a@gmail.com>
2016-10-09 19:24:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var image = document.getElementById('madi_img');
 var marginLeft=0;
 function shiftImageRight(){
-    marginLeft= marginLeft+10;
+    marginLeft= marginLeft+5;
     image.style.marginLeft =marginLeft+'px';
 }
 image.onclick = function () {
-    setInterval(shiftImageRight,100)
+    setInterval(shiftImageRight,50)
 };
