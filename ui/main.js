7146e4cf55fe9cc98360e3c6d71f978b849b6a06
Hariraj <raja70a@gmail.com>
2016-10-09 19:28:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ function shiftImageRight(){
     marginLeft= marginLeft+5;
     image.style.marginLeft =marginLeft+'px';
 }
-image.onclick = function () {
+image.onclick=function () {
     setInterval(shiftImageRight,50)
 };
