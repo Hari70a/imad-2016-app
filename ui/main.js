0a97fdb8e61a46743bd1fe6499c0ed985a6bdffd
Hariraj <raja70a@gmail.com>
2016-10-09 20:40:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var submit = document.getElementById('submit_btn')
 var counter =0;
 submit.onclick=function(){
     console.log("INN")
-    counter= counter+1;
-    console.log(counter,"####")
+    counter=counter+1;
+    var span = document.getElementById('span')
+    span.value = counter.toString();
 };
