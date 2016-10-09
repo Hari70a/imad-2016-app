bdb45184052b0627df9335fe34c0267c847f0b38
Hariraj <raja70a@gmail.com>
2016-10-09 19:23:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 var image = document.getElementById('madi_img');
+var marginLeft=0;
 function shiftImageRight(){
     marginLeft= marginLeft+10;
     image.style.marginLeft =marginLeft+'px';
