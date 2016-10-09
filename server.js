c1fd89a38f9b6b0b7ce78acf27d92a60b477f769
Hariraj <raja70a@gmail.com>
2016-10-09 14:21:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-  res.send("Article1 is presented");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article_two', function (req, res) {
