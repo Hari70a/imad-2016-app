11d11322bbe07e2d84b31ab5f66f2ff99d789c7f
Hariraj <raja70a@gmail.com>
2016-11-06 23:10:05 +0530

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<title>Fascinating Blogs</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" href="assets/css/main.css" />
-<!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/skel.min.js"></script>
-<script src="assets/js/util.js"></script>
-<script src="assets/js/main.js"></script>
